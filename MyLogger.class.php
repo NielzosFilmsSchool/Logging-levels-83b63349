ef6ceeea28c8520143278a16881bf8e7a2fa718a
NielzosFilmsSchool <niels.hazelaar@student.rocva.nl>
2020-03-04 09:09:19 +0100

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -1,33 +1,39 @@
 <?php
 //`warning`, `error`, `info`, `debug`
-class MyLogger {
-    public function log($message, $level){
+class MyLogger
+{
+    public function log($message, $level)
+    {
         switch($level){
-            case "WARNING":
-                $this->warning($message);
-                break;
-            case "ERROR":
-                $this->error($message);
-                break;
-            case "INFO":
-                $this->info($message);
-                break;
-            case "DEBUG":
-                $this->warning($message);
-                break;
+        case "WARNING":
+            $this->warning($message);
+            break;
+        case "ERROR":
+            $this->error($message);
+            break;
+        case "INFO":
+            $this->info($message);
+            break;
+        case "DEBUG":
+            $this->warning($message);
+            break;
         }
     }
 
-    public function warning($message){
+    public function warning($message)
+    {
         echo "WARNING: ".$message.PHP_EOL;
     }
-    public function error($message){
+    public function error($message)
+    {
         echo "ERROR: ".$message.PHP_EOL;
     }
-    public function info($message){
+    public function info($message)
+    {
         echo "INFO: ".$message.PHP_EOL;
     }
-    public function debug($message){
+    public function debug($message)
+    {
         echo "DEBUG: ".$message.PHP_EOL;
     }
 }
